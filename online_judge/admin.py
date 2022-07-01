e0ac72b41093fbc8c0539987884b5a3703704f26
har3927 <harsha.yella2002@gmail.com>
2022-07-01 21:28:56 +0530

    changes in models and admin has been added

--- online_judge/admin.py
+++ online_judge/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Users,Problems,Solutions,TestCases
 # Register your models here.
+admin.site.register(Users)
+admin.site.register(Problems)
+admin.site.register(Solutions)
+admin.site.register(TestCases)
--- online_judge/models.py
+++ online_judge/models.py
@@ -1,10 +1,28 @@
 from django.db import models
 
+#choices
+Languages = (
+    ('py', 'Python'),
+    ('cpp', 'C++'),
+    ('c', 'C Language'),
+    ('java', 'Java'),
+)
+
+Verdicts = (
+    ('ACC', 'Accepted'),
+    ('TLE', 'Time Limit Exceeded'),
+    ('WA', 'Wrong Answer'),
+    ('MLE', 'Memory Limit Exceeded'),
+)
+
 # Create your models here.
+
 #table 1
 class Users(models.Model):
+    user_name=models.CharField(max_length=100)
     password = models.CharField(max_length=50)
     score = models.IntegerField(default = 0)
+    rank=models.IntegerField(default=0)
     submissions = models.IntegerField(default=0)
 #table 2
 class Problems(models.Model):
@@ -12,15 +30,21 @@ class Problems(models.Model):
     name = models.CharField(max_length=200)
     code = models.CharField(max_length=1200)
     difficulty = models.CharField(max_length=10)
+    def __str__(self):
+        return self.name
 #table 3
 class Solutions(models.Model):
     problem = models.ForeignKey(Problems, on_delete=models.CASCADE)
-    user = models.ForeignKey(Users, on_delete=models.CASCADE)
-    language = models.CharField(max_length = 10)
-    verdict = models.CharField(max_length=20)
+    language = models.CharField(max_length = 10,choices=Languages)
+    verdict = models.CharField(max_length=20,choices=Verdicts)
     submitted_at = models.DateTimeField('Submitted On')
+    def was_published_recently(self):
+        return (self.submitted_at>=timezone.now()- datetime.timedelta(days=1))
+
 #table 4
 class TestCases(models.Model):
     input = models.CharField(max_length=200)
     output = models.CharField(max_length=200)
     problem = models.ForeignKey(Problems, on_delete=models.CASCADE)
+    def __str__(self):
+        return "test case {} for problem {}".format(self.pk, self.name)
