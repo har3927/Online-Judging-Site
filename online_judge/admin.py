374f44ebdf4f9a886cf122c289ab0cb965c2abcd
har3927 <harsha.yella2002@gmail.com>
2022-07-08 19:45:33 +0530

    leader template and submit view has been added

--- online_judge/admin.py
+++ online_judge/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Problem,TestCase
+from .models import Problem,TestCase,Solution
 # Register your models here.
 admin.site.register(Problem)
+admin.site.register(Solution)
 admin.site.register(TestCase)
--- online_judge/views.py
+++ online_judge/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import get_object_or_404, render,HttpResponse
-
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, redirect, render,HttpResponse
+from django.utils import timezone
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .models import Problem, Solution
+import filecmp, os
 
 # Create your views here.
 
@@ -14,8 +19,50 @@ def problemDetails(request,problem_id):
     context = { 'problem':problem}
     return render(request,'online_judge/problemDetails.html', context)
 
-def submitProblem(request):
-    return HttpResponse("Submit Problem Page")
 
+@login_required(login_url='members:login')
+def submitProblem(request,problem_id):
+    f = request.FILES.get('solution' , False)
+    a = request.POST['typedsol']
+    if(f):
+        with open('/harsh/solution.cpp','wb+') as dest:
+            for chunk in f.chunks():
+                dest.write(chunk)
+    elif(len(a) != 0):
+        with open('/harsh/solution.cpp','w') as dest:
+            dest.write(a)
+    else:
+        problem = get_object_or_404(Problem, pk=problem_id)
+        messages.info(request, "Please select a file or write code in textarea ")
+        context = { 'problem':problem}
+        return render(request,'online_judge/problemDetails.html', context)
+
+    os.system('g++ /harsh/solution.cpp ')
+    os.system('a.exe < /harsh/input.txt > /harsh/out.txt')
+
+
+    out1 = '/harsh/out.txt'
+    out2 = '/harsh/acc_out.txt'
+
+    if(filecmp.cmp(out1,out2,shallow=False)):
+        messages.success(request, 'Your solution was accepted..')
+        verdict = 'Accepted'
+    else:
+        messages.info(request, 'Your solution was not accepted..')
+        verdict = 'Wrong answer'
+
+    solution = Solution()
+    solution.language = 'cpp'
+    solution.problem = Problem.objects.get(pk=problem_id)
+    solution.verdict = verdict
+    solution.submitted_at = timezone.now()
+    solution.submitted_code = '/harsh/solution.cpp'
+    solution.save()
+
+    return HttpResponseRedirect(reverse('online_judge:leaderboard'))
+
+
+@login_required(login_url='users:login')
 def leaderboard(request):
-    return HttpResponse("Leaderboard Page")
+    solutions = Solution.objects.all()
+    return render(request, 'online_judge/leaderboard.html', {'solutions' : solutions})
